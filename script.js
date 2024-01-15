189e85e89268f71f26bf32a8c2e6985986e46746
NokaK <nokakipiani93@gmail.com>
2024-01-15 21:13:26 +0300

    add variables

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Frond end development!!!");
+console.log("Hello World!!!");
+let name = "Sarath";
+let surname = "Kumar"; //string
+let age = 18; //number
+console.log(name, surname, age);
+console.log("sarah kumar is 18 age old");
+console.log(name + " is " + surname + " " + age + " age old");
+let sentence = `${name} is ${surname} ${age} age old`;
+let isMarried = null; // boolean
+isMarried = false;
+
+console.log(typeof surname);
+
+let person = {
+  username: "Sarath",
+  surname: "Kumar",
+  age: 18,
+  isMarried: false,
+};
+let sentence2 = `${person.username} is ${person.surname} ${person.age} age old`;
+
+let x = 50;
+let y = 20 * 2;
+let c = "50";
+let z = x + y + c;
